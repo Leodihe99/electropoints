b5628842e0abfac3ab3e7ecf0e59786cb2b4ce8f
Ivan Diaz <ivan.diaz@orasgroup.co>
2022-10-12 18:09:58 -0500

    Adelantos clases 11 octubre 2022

--- app/src/main/java/com/grupo1z10/electropoints/MainActivity.java
+++ app/src/main/java/com/grupo1z10/electropoints/MainActivity.java
@@ -3,12 +3,22 @@ package com.grupo1z10.electropoints;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Lugar lugarNuevo= new Lugar("Casa", "Carrera 68g 9c-51",
+                -73.121, 7.1377, TipoLugar.GASOLINERA, 30577,
+                "http","", 5.0F);
+        mensaje = findViewById(R.id.mensaje);
+        mensaje.setText(lugarNuevo.toString());
+        Log.d("TAG", "Objeto lugar encontrado"+ lugarNuevo.toString());
     }
 }
